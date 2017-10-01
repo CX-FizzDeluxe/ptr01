0623ffd98c03a75242252d40317da2e240c910e9
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-10-01 04:03:16 +0100

    Sun Oct 01 04:03:16 BST 2017

--- src/main/java/befaster/solutions/FizzBuzz.java
+++ src/main/java/befaster/solutions/FizzBuzz.java
@@ -10,12 +10,22 @@ public class FizzBuzz {
 
     public static String fizzBuzz(Integer number) {
         String numberString = number.toString();
-        boolean isAllMatch = Arrays.asList(string.toCharArray()).stream().collect(Collectors.toSet());
-        boolean isDeluxe = number > 0 && isAllMatch(numberString);
+        boolean isAllMatch = Arrays.asList(numberString.toCharArray())
+                .stream()
+                .collect(Collectors.toSet())
+                .size() == 1;
+
+        boolean isDeluxe = number > 0 && isAllMatch;
         boolean isFizz = number % 3 == 0 || numberString.contains("3");
         boolean isBuzz = number % 5 == 0 || numberString.contains("5");
+        if (isFizz && isBuzz && isDeluxe) {
+            return "fizz buzz deluxe";
+        }
         if (isFizz && isBuzz) {
             return "fizz buzz";
+        }
+        if (isDeluxe) {
+            return "deluxe";
         }
         if (isFizz) {
             return "fizz";
