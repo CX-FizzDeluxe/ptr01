04d27ec101d6eaf3fa2f68a4a163c3419bfb873a
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-10-01 04:12:16 +0100

    Sun Oct 01 04:12:16 BST 2017

--- src/main/java/befaster/solutions/FizzBuzz.java
+++ src/main/java/befaster/solutions/FizzBuzz.java
@@ -11,8 +11,6 @@ public class FizzBuzz {
 
     public static String fizzBuzz(Integer number) {
         String numberString = number.toString();
-        
-
         boolean isDeluxe = number > 10 && isAllMatch(numberString);
         boolean isFizz = number % 3 == 0 || numberString.contains("3");
         boolean isBuzz = number % 5 == 0 || numberString.contains("5");
@@ -21,9 +19,6 @@ public class FizzBuzz {
         }
         if (isFizz && isBuzz) {
             return "fizz buzz";
-        }
-        if (isDeluxe) {
-            return "deluxe";
         }
         if (isFizz) {
             return "fizz";
