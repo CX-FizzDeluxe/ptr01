2fb86db2347d287de4cfeeba125e1bb6eb39c208
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-10-01 03:59:11 +0100

    Sun Oct 01 03:59:11 BST 2017

--- src/main/java/befaster/solutions/FizzBuzz.java
+++ src/main/java/befaster/solutions/FizzBuzz.java
@@ -1,11 +1,17 @@
 package befaster.solutions;
 
 import befaster.runner.SolutionNotImplementedException;
+import java.util.Arrays;
+import java.util.HashSet;
 
 public class FizzBuzz {
 
     public static String fizzBuzz(Integer number) {
         String numberString = number.toString();
+        
+        boolean isAllMatch = (new HashSet<char>(Arrays.asList(numberString.toCharArray()))).size() == 1;
+        
+        boolean isDeluxe = number > 0;
         boolean isFizz = number % 3 == 0 || numberString.contains("3");
         boolean isBuzz = number % 5 == 0 || numberString.contains("5");
         if (isFizz && isBuzz) {
